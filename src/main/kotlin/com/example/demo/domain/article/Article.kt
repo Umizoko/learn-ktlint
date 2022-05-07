27f32991b1c950871a6f5c9cbf076e7dc3031124
Maruyama Kai <0123mark3210@gmail.com>
2022-05-08 00:22:08 +0900

    illegal code style

---
+++ src/main/kotlin/com/example/demo/domain/article/Article.kt
@@ -0,0 +1,12 @@
+package com.example.demo.domain.article
+
+class Article private constructor(
+    val id: String,
+    var title: String,
+    val description: String
+        ){
+
+    fun changeTitle(title: String) {
+        this.title = title
+    }
+}
